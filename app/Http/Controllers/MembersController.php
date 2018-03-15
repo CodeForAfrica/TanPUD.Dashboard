4e2f8744e83307778b53aabc052ea86e5ba9babd
Timothy Anthony <anthony.timothy90@gmail.com>
2018-03-15 08:48:32 +0300

    Initial deployment

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -9,18 +9,33 @@ class MembersController extends Controller
 {
     public function index()
     {
-         // Checking for session.
-         if(!Auth::check())
-         {
-             return redirect('login');
-         }
-         else{
-             $user = Auth::user();
+        // Checking for session.
+        if(!Auth::check())
+        {
+            return redirect('login');
+        }
+        else{
+            $user = Auth::user();
+
+            // Fetching members from ODK Aggegate.
+            $host = env('ODK_HOSTNAME','');
+            $user = env('ODK_USER','');
+            $pass = env('ODK_PASSWORD','');
+            $db = env('ODK_DATABASE','');
+
+            $link = mysqli_connect($host, $user, $pass, $db);
+
+            if(!$link) {
+                echo "Error: Unable to connect to MySQL." . PHP_EOL;
+                echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+                exit;
+            }
  
-             $data = array(
-                 'page' => 'Members'
-             );
-             return view('admin.members.main',compact('user','data'));
-         }
+            $data = array(
+                'page' => 'Members'
+            );
+            return view('admin.members.main',compact('user','data'));
+        }
     }
 }
