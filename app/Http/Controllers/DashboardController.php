3c72f5cfe32bbee1462ecbda2506e6dcce02ea27
Timothy Anthony <anthony.timothy90@gmail.com>
2018-03-19 05:47:33 +0300

    Counting number of members and disputes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,13 +19,101 @@ class DashboardController extends Controller
         else{
             $user = Auth::user();
 
-            $total_members = Member::count();
+            $total_members = count($this->getMembers());
+            $total_disputes = count($this->getDisputes());
 
             $data = array(
                 'page' => 'Dashboard',
-                'total_members' => $total_members
+                'total_members' => $total_members,
+                'total_disputes' => $total_disputes
             );
             return view('admin.dashboard',compact('user','data'));
+        }
+    }
+
+    public function getMembers($firstname = "", $middlename = "", $surname = ""){
+        $client = new \GuzzleHttp\Client(['http_errors' => true]);
+
+        $url = "http://54.76.5.187:8990/restsql/res/Member?_output=application/json&_limit=50&_offset=0";
+
+        if($firstname != "" && $middlename != "" && $surname != ""){
+            $url .= "&FIRSTNAME=";
+            $url .= $firstname;
+            $url .= "&MIDDLENAME=";
+            $url .= $middlename;
+            $url .= "&SURNAME=";
+            $url .= $surname;
+        }
+
+        try{
+            $response = $client->request('GET', $url);
+            $response_json = json_decode($response->getBody());
+
+            if($response_json->members)
+            {
+                return $response_json->members;
+            }
+            else{
+                // No Users.
+                return null;
+            }
+        }
+        catch (ClientErrorResponseException $e) {
+            \Log::info("Client error :" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (ServerErrorResponseException $e) {
+            \Log::info("Server error" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (BadResponseException $e) {
+            \Log::info("BadResponse error" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (\Exception $e) {
+            \Log::info("Err" . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function getDisputes($uri = ""){
+        $client = new \GuzzleHttp\Client(['http_errors' => true]);
+
+        $url = "http://54.76.5.187:8990/restsql/res/Dispute?_output=application/json&_limit=50&_offset=0";
+
+        if($uri != ""){
+            $url .= "&_URI=";
+            $url .= $uri;
+        }
+
+        try{
+            $response = $client->request('GET', $url);
+            $response_json = json_decode($response->getBody());
+
+            if($response_json->disputes)
+            {
+                return $response_json->disputes;
+            }
+            else{
+                // No Users.
+                return null;
+            }
+        }
+        catch (ClientErrorResponseException $e) {
+            \Log::info("Client error :" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (ServerErrorResponseException $e) {
+            \Log::info("Server error" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (BadResponseException $e) {
+            \Log::info("BadResponse error" . $e->getResponse()->getBody(true));
+            return null;
+        }
+        catch (\Exception $e) {
+            \Log::info("Err" . $e->getMessage());
+            return null;
         }
     }
 }
